9b02518ab6590aefc81806532ccbf42044229b67
Norisora <soul05537@yahoo.co.jp>
2023-04-24 07:40:06 +0900

    p194まで

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,7 +15,9 @@ public class GameManager : MonoBehaviour
     Image titleImage;       //画像を表示しているImageコンポーネント
 
     // +++ 時間制限追加 +++
-    public GameObject timeBar;
+    public GameObject timeBar;      //時間表示Image
+    public GameObject timeText;     //時間Text
+    TimeController timeCnt;         //TimeController
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,17 @@ public class GameManager : MonoBehaviour
         Invoke("InactiveImage", 1.0f);
         //ボタン（パネル）を非表示にする
         panel.SetActive(false);
+
+        // +++ 時間制限追加 +++
+        //TimeControllerを取得
+        timeCnt = GetComponent<TimeController>();
+        if (timeCnt != null)
+        {
+            if (timeCnt.gameTime == 0.0f)
+            {
+                timeBar.SetActive(false);   //制限時間なしなら隠す
+            }
+        }
     }
 
     // Update is called once per frame
@@ -38,6 +51,12 @@ public class GameManager : MonoBehaviour
             panel.SetActive(true);
             mainImage.GetComponent<Image>().sprite = gameClearSpr;  //画像を設定する
             PlayerController.gameState = "gameend";
+
+            // +++ 時間制限追加 +++
+            if (timeCnt != null)
+            {
+                timeCnt.isTimeOver = true;  //時間カウント停止
+            }
         }
         else if (PlayerController.gameState == "gameover")
         {
@@ -49,10 +68,36 @@ public class GameManager : MonoBehaviour
             panel.SetActive(true);
             mainImage.GetComponent<Image>().sprite = gameOverSpr;
             PlayerController.gameState = "gameend";
+
+            // +++ 時間制限追加 +++
+            if (timeCnt != null) 
+            {
+                timeCnt.isTimeOver = true;  //時間カウント停止
+            }
         }
         else if (PlayerController.gameState == "playing")
         {
             //ゲーム中
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            //PlayerControllerを取得する
+            PlayerController playerCnt = player.GetComponent<PlayerController>();
+            // +++ 時間制限追加 +++
+            //タイムを更新する
+            if (timeCnt != null)
+            {
+                if (timeCnt.gameTime > 0.0f)
+                {
+                    //整数に代入することで小数点を切り捨てる
+                    int time = (int)timeCnt.displayTime;
+                    //タイム更新
+                    timeText.GetComponent<Text>().text = time.ToString();
+                    //タイムオーバー
+                    if (time == 0) 
+                    {
+                        playerCnt.GameOver();   //ゲームオーバーにする
+                    }
+                }
+            }
         }
     }
     //画像を非表示にする
